f4b8717ec350b57300191f7f3a9573584b0f28e0
Negar <nemote95@gmail.com>
2020-03-30 12:15:42 +1300

    used csv/ pandas dataframe for managing the data

--- covid19.py
+++ covid19.py
@@ -3,6 +3,7 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 url = 'https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases'
 
@@ -10,17 +11,18 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text,  "html.parser")
 
+df_file_name = "covid19-nz.csv"
+df = pd.read_csv(df_file_name)
 
-f = open("covid19-nz.csv","a+")
+tds = soup.find_all("td")
 
+today_cases = tds[2].text
+total_cases = tds[1].text
 
-numbers =""
-lis = soup.find_all("td")[1:3]
-for i in lis :
-    numbers+= ","+ i.text
+report_date = soup.find_all("p",class_="georgia-italic")[0].text.split(",")[1].strip(".")
 
-date = soup.find_all("p",class_="georgia-italic")[0].text.split(",")[1].strip(".")
-
-f.write(date+numbers+"\n")
-f.close()
+new_report = {"Date":report_date, "Total":total_cases, "Last24":today_cases}
+df = df.append([new_report])
+print(df)
+df.to_csv(df_file_name, index=False)
 
