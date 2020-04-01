145288e4959d1046665cb6ead29434f843ef13c5
Negar <nemote95@gmail.com>
2020-04-01 14:43:01 +1300

    added plotting and linear regression

--- covid19.py
+++ covid19.py
@@ -26,9 +26,34 @@ new_report = {"Date":report_date, "Total":total_cases, "Last24":today_cases}
 #save the new report
 df_file_name = "covid19-nz.csv"
 df = pd.read_csv(df_file_name)
-
+print(new_report)
 #to avoid repeating reports
 if report_date not in df.Date.values:
     df = df.append([new_report])
     df.to_csv(df_file_name, index=False)
+
+
+#update regression
+from sklearn.linear_model import LinearRegression
+X = df.index.values.reshape(-1,1)
+y = df['Last24']
+
+print(X.shape,y.shape)
+regr = LinearRegression()
+model = regr.fit(X , y)
+
+
+#visualization
+import matplotlib.pyplot as plt
+
+predicted_y = regr.predict(X)
+
+plt.scatter(df['Date'], y,  color='black')
+plt.plot(df['Date'], predicted_y, color='blue', linewidth=3)
+
+plt.xlabel("Date")
+plt.ylabel("Daily new cases")
+
+
+plt.show()
 
