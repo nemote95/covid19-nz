3a40c58338f1c9819730522748c06bea270d5533
Negar <nemote95@gmail.com>
2020-05-16 00:01:23 +1200

    fixed date formats

--- covid19.py
+++ covid19.py
@@ -4,6 +4,8 @@ import time
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
+from datetime import datetime
+
 
 #sending request to the webpage
 url = 'https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases'
@@ -19,17 +21,20 @@ today_cases = tds[1].text
 total_cases = tds[0].text
 
 report_date = soup.find_all("p",class_="georgia-italic")[0].text.split(",")[1].strip(".")
+report_date = datetime.strptime(report_date.lstrip(), '%d %B %Y')
 
 #creating a new record for the new report
-new_report = {"Date":report_date, "Total":total_cases, "Last24":today_cases}
+
+new_report = {"Date":datetime.strftime(report_date,"%d/%m/%Y"), "Total":total_cases, "Last24":today_cases}
 
 #save the new report
 df_file_name = "covid19-nz.csv"
 df = pd.read_csv(df_file_name)
 print(new_report)
 #to avoid repeating reports
-if report_date not in df.Date.values:
+if new_report["Date"] not in df["Date"].values:
     df = df.append([new_report])
+    df.Date = df["Date"]
     df.to_csv(df_file_name, index=False)
 
 
@@ -48,8 +53,8 @@ import matplotlib.pyplot as plt
 
 predicted_y = regr.predict(X)
 
-plt.scatter(df['Date'], y,  color='black')
-plt.plot(df['Date'], predicted_y, color='blue', linewidth=3)
+plt.scatter(df['Date'].astype(str), y,  color='black')
+plt.plot(df['Date'].astype(str), predicted_y, color='blue', linewidth=3)
 
 plt.xlabel("Date")
 plt.ylabel("Daily new cases")
